982997001611535df5abe34d71097fdd921eeeed
Austin <applecat69@gmail.com>
2024-10-23 17:42:50 +0800

    Add files via upload
    debug

--- components/Results.js
+++ components/Results.js
@@ -1,8 +1,11 @@
 import styles from '../styles/Results.module.css'
+import { useTranslation } from 'next-i18next'
 
 export default function Results({ results }) {
+  const { t } = useTranslation('common')
+
   return (
-    <div className={styles.results}>
+    <div>
       <h2>{t('resultsTitle')}</h2>
       <ul>
         {results.map((result, index) => (
