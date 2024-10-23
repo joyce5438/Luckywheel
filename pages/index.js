2565f20509d7565b05dd64148e552aafecac0498
Austin <applecat69@gmail.com>
2024-10-23 18:19:16 +0800

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
